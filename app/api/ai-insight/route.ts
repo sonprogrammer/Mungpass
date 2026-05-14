d411675494e234a2714aba2712fa3031dc2984e6
sonprogrammer <ods04138@gmail.com>
2026-05-14 22:07:25 +0900

    fix: build error

--- app/api/ai-insight/route.ts
+++ app/api/ai-insight/route.ts
@@ -3,6 +3,8 @@ import { NextRequest, NextResponse } from "next/server"
 import { supabaseServer } from "@/shared/api/supabase/server"
 import { format } from "date-fns"
 
+console.log("GEMINI KEY:", process.env.GEMINI_API_KEY);
+
 const geminiAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
 
 export async function POST(req: NextRequest){
