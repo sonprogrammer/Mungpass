fe207fbbe496343880bc030086c3f97016c079aa
sonprogrammer <ods04138@gmail.com>
2026-05-14 20:44:16 +0900

    fix: build error

--- app/(auth)/signup/(role)/owner/layout.tsx
+++ app/(auth)/signup/(role)/owner/layout.tsx
@@ -1,11 +1,13 @@
 import { StepBar } from "@/features/auth/ui/owner/StepBar";
+import { Suspense } from "react";
 
 export default function OwnerSignupLayout({children}: {children: React.ReactNode}){
     return(
         <div className="w-full  pt-3 h-full flex flex-col">
             <header className="pb-2">
-                {/* <h1 className="text-center font-black text-xl mb-4">사장님 회원가입</h1> */}
-                <StepBar />
+                <Suspense fallback={null}>
+                    <StepBar />
+                </Suspense>
             </header>
             <main className="flex-1 overflow-y-auto">
                 {children}
