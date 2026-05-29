77a6ec67fe53bed29d84ff98bd30682ad028b47a
sonprogrammer <ods04138@gmail.com>
2026-05-29 18:07:24 +0900

    refactor: clean up unnecessary code

--- features/auth/model/useAuthForm.tsx
+++ features/auth/model/useAuthForm.tsx
@@ -1,28 +1,16 @@
 'use client'
 import { signup } from "../api/signup";
 import { regularLogin } from "../api/regularLogin";
-import { useRouter } from "next/navigation";
 import { App } from "antd";
 
 export function useAuthForm(mode: 'login' | 'signup', OwnerSuccess?: (id: string) => void) {
-  const router = useRouter()
 
   const { message } = App.useApp()
 
   const handleAuthAction = async (formData: FormData) => {
     if (mode ==='login') {
       try {
-        const user = await regularLogin(formData)
-        const role = user.actualRole
-        
-        
-        // if(role === 'admin'){
-        //   router.replace('/admin')
-        // }else if(role === 'owner'){
-        //   router.replace('/owner')
-        // }else{
-        //   router.replace('/home')
-        // }
+        await regularLogin(formData)
       } catch(error: unknown) {
         message.error(error instanceof Error ? error.message : '로그인 중 에러가 발생하였습니다. 다시 시도해주세요')
       }
