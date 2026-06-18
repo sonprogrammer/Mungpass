0e48c117e42cd5bc68629459282d2a5d80c412da
sonprogrammer <ods04138@gmail.com>
2026-06-18 22:08:17 +0900

    feat: pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LayoutContent from "@/app/ui/LayoutContent";
@@ -50,6 +50,13 @@ export const metadata: Metadata = {
   icons: {
     apple: '/logo-512.png'
   }
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
